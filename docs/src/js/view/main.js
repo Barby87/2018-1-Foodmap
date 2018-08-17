77d5cfccac5c8b265f5cebb49b9c2c6dd8df1117
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-08-17 16:45:46 -0300

    Preparando deploy

--- docs/src/js/view/main.js
+++ docs/src/js/view/main.js
@@ -1,13 +1,14 @@
-/*window.onload = () => {
+window.onload = () => {
 	setTimeout(function(){
     const preload = document.getElementById('preload');
     const home = document.getElementById('home');
     preload.style.display = 'none';
     home.style.display = 'block';
-  },4000);
-  
-}*/
+  },3000);
 
+}
+
+// Inicializando la comunicación con los servicios de back-end
 var platform = new H.service.Platform({
   'app_id': 'SvaVFK30FoK6MSSeVH5e ',
   'app_code': 'Q2xoQ6Mumd9vR2Yo2KYQ2Q '
@@ -25,15 +26,19 @@ var map = new H.Map(
     center: { lat: 52.5, lng: 13.4 }
   });
 
+  
+  // Función para obtener posición actual del dispositivo
   function updatePosition (event) {
     var HEREHQcoordinates = {
       lat: event.coords.latitude,
       lng: event.coords.longitude
     };
   
+    // Declarando variable marcador
     var marker = new H.map.Marker(HEREHQcoordinates);
     map.addObject(marker);
     map.setCenter(HEREHQcoordinates);
   }
   
-  navigator.geolocation.watchPosition(updatePosition);
+  // El método watchPosition llamará a la función de éxito cada vez que cambie la posición del dispositivo.
+  navigator.geolocation.watchPosition(updatePosition); 
